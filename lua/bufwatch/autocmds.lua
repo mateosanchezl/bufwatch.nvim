8107f007cb1d015afd249a7ff04ef17ea3d3f65a
mateo <mateosanchez.msl@gmail.com>
2025-12-14 17:40:12 -0400

    autocmds, better mem state

---
+++ lua/bufwatch/autocmds.lua
@@ -0,0 +1,8 @@
+local group = vim.api.nvim_create_augroup("bufwatch", { clear = true })
+
+vim.api.nvim_create_autocmd({ "BufEnter", "BufLeave" }, {
+	group = group,
+	callback = function(event)
+		require("bufwatch").on_event(event)
+	end,
+})
--- lua/bufwatch/init.lua
+++ lua/bufwatch/init.lua
@@ -1,87 +1,106 @@
 local M = {}
 
 local state = {
-	current_project = {
-		root = "",
-		current_file = "",
-
-		start_time = nil,
-		total_elapsed_time = nil,
-	},
+	projects = {},
+	current_root = "",
+	current_file_path = "",
 }
 
-local function set_project_root()
-	if state.current_project.root ~= nil then
+local function register_project_root()
+	if state.current_root ~= "" then
+		-- Get project root
 		local cwd = vim.fn.fnamemodify(vim.fn.expand("%"), ":p:h")
 		local git_root = vim.fn.systemlist("git -C " .. cwd .. " rev-parse --show-toplevel")[1]
 		if vim.v.shell_error ~= 0 or not git_root then
 			print("Error setting project root")
 			return
 		end
-		state.current_project.root = git_root
+
+		state.current_root = git_root
+		-- Check if set
+		if not state.projects[git_root] then
+			state.projects[git_root] = {
+				files = {},
+				metadata = {},
+			}
+		end
 	else
 		-- Already set
 		return
 	end
 end
 
-local function resolve_current_file()
-	state.current_project.current_file = vim.api.nvim_buf_get_name(0)
-end
-
-function M.on_buf_enter()
-	set_project_root()
-	resolve_current_file()
-
-	if state.current_project.current_file == "" or state.current_project.root == "" then
-		print("Error getting current file or project")
-		return
+local function is_real_file(bufnr)
+	if vim.bo[bufnr].buftype ~= "" then
+		return false
 	end
 
-	state.current_project.start_time = os.time()
+	local name = vim.api.nvim_buf_get_name(bufnr)
+	if name == "" then
+		return false
+	end
+
+	return true
 end
 
--- Calculates and saves total elapsed time in-state
+local function register_current_file()
+	local file_path = vim.api.nvim_buf_get_name(0)
+	state.current_file_path = file_path
+
+	local curr_project = state.projects[state.current_root]
+	if not curr_project then
+		error("Couldn't find this project in memory")
+	end
+
+	if not curr_project.files[file_path] then
+		curr_project.files[file_path] = {
+			total_time_s = 0,
+			last_start = nil,
+		}
+	end
+end
+
+local function register_buffer()
+	register_project_root()
+	register_current_file()
+end
+
+local function on_buf_enter()
+	if not is_real_file(0) then
+		return
+	end
+	register_buffer()
+
+	local s = state.current_project
+	if s.current_file == "" or s.root == "" then
+		error("Error getting current file or project")
+		return
+	end
+end
+
+-- Calculates and saves total elapsed time in-memory
 local function sync_elapsed_time()
-	local s = state.current_project
+	local last_start = state.projects[state.current_root].files[state.current_file_path].last_start
 
-	local last_elapsed = os.time() - s.start_time
-	if s.total_elapsed_time > 0 then
-		s.total_elapsed_time = s.total_elapsed_time + last_elapsed
-	else
-		s.total_elapsed_time = last_elapsed
+	if last_start > 0 then
+		local elapsed = os.time()
+		print("Elapsed: ")
+		state.projects[state.current_root].files[state.current_file_path].total_time_s = last_start + elapsed
+		last_start = 0
 	end
 end
 
-function M.on_buf_unfocus()
+local function on_buf_leave()
 	sync_elapsed_time()
 end
 
-function M.on_buf_refocus()
-	local s = state.current_project
-	s.start_time = os.time()
-end
-
-function M.on_buf_leave()
-	if state.current_project.start_time == nil then
-		return
+function M.on_event(event)
+	print(event)
+	if event == "BufEnter" then
+		on_buf_enter()
+	elseif event == "BufLeave" then
+		on_buf_leave()
 	end
-
-	sync_elapsed_time()
-	-- Save to JSON or something
-	-- eg:
-	-- {
-	--  project: "/Desktop/repo..."
-	--  file_stats: {
-	--    repo/init.lua: {
-	--      total_elapsed_time: 15214
-	--      last_reset:
-	--    }
-	--  }
-	-- }
-
-	-- Clear state
-	state.current_project = { root = "", current_file = "", start_time = nil, total_elapsed_time = nil }
 end
 
 return M
